6f946af5e877dca04e858d2e97df60ebdbe81fc8
Michael Egger <me@anyma.ch>
2020-02-12 10:54:31 +0100

    implement servo protocol, logging, statistics

--- Changlier-Firmware/Changlier-Firmware.ino
+++ Changlier-Firmware/Changlier-Firmware.ino
@@ -1,4 +1,4 @@
-#define VERSION "2019-12-03"
+#define VERSION "2020-02-12"
 
 //----------------------------------------------------------------------------------------
 //
@@ -9,23 +9,16 @@
 //		License: 	This is FREE software (as in free speech, not necessarily free beer)
 //					published under gnu GPL v.3
 //
-// !!!!!!!!!!!!!!!! 
-// !!!!    Input functionality requires replacing the esp32-hal-uart.c
-// !!!!  	files in Arduino/hardware/espressif/esp32/cores/esp32/
-// !!!!!!!!!!!!!!!! 
-//
 //----------------------------------------------------------------------------------------
 
 
 #include <Preferences.h>
-#include "Timer.h"
 #include "Password.h"
 #include "WiFi.h"
 #include <ESPmDNS.h>
 #include <SPIFFS.h>
 #include "ESPAsyncWebServer.h"
 #include <ESP32Servo.h>
-//#include "AppleMidi.h"
 #include <WiFiUdp.h>
 #include "CircularBuffer.h"
 
@@ -35,41 +28,46 @@
 
 // .............................................................................Pins 
 
+
 const char 	servo_pins[] 			= {32,33,25,26,27,14};
 const char 	BTN_SEL 				= 3;	// Select button
 const char 	BTN_UP 					= 1; // Up
 
 const char NUM_SERVOS				= 6;
+const int LOG_SIZE					= 500;
 
-const char * udpAddress = "10.0.0.3";
+const char * udpAddress = "192.168.0.255";
 const int udpPort = 44444;
-WiFiUDP udp;
+const int WIFI_TIMEOUT		=			4000;
 
 
 //========================================================================================
 //----------------------------------------------------------------------------------------
 //																				GLOBALS
 Preferences                             preferences;
-Timer									t;
 Servo 									myservo[NUM_SERVOS];
 
-CircularBuffer<byte, 100> queue;
+//CircularBuffer<byte, 100> queue;
 
-
-// .............................................................................WIFI STUFF 
-#define WIFI_TIMEOUT					4000
 String 									hostname;
 AsyncWebServer                          server(80);
-//APPLEMIDI_CREATE_INSTANCE(WiFiUDP, AppleMIDI); // see definition in AppleMidi_Defs.h
+WiFiUDP 								udp;
 
-char servo_val_raw[6];
+
+char servo_val_raw[NUM_SERVOS];
 long last_packet;
+long servo_packet_interval;
+long servo_packet_interval_avg;
+long servo_packet_interval_min;
+long servo_packet_interval_max;
+long last_servo_service;
+CircularBuffer<unsigned int, LOG_SIZE> 	servo_packet_interval_log;
+
+long servo_service_interval = 4000; // microseconds
 boolean isConnected;
 
-char servo_address[NUM_SERVOS];
 char servo_minimum[NUM_SERVOS];
 char servo_maximum[NUM_SERVOS];
-char servo_detach_address[NUM_SERVOS];
 
 //========================================================================================
 //----------------------------------------------------------------------------------------
@@ -78,23 +76,13 @@ char servo_detach_address[NUM_SERVOS];
 //----------------------------------------------------------------------------------------
 //																				Preferences
 
-void setup_read_preferences() {
+void read_preferences() {
     preferences.begin("changlier", false);
 
     hostname = preferences.getString("hostname");
     if (hostname == String()) { hostname = "changlier"; }
     Serial.print("Hostname: ");
     Serial.println(hostname);
-
-   if(preferences.getBytesLength("addresses") != NUM_SERVOS) {
-    	Serial.println(F("Generating default adresses"));
-    	for (int i = 0; i < NUM_SERVOS; i++) {
-    		servo_address[i] = i+1;
-    	}
-    	preferences.putBytes("addresses",servo_address,NUM_SERVOS);
-    } else {
-        preferences.getBytes("addresses",servo_address,NUM_SERVOS);
-    }
     
    if(preferences.getBytesLength("minima") != NUM_SERVOS) {
     	Serial.println(F("Generating default minima + maxima"));
@@ -108,17 +96,6 @@ void setup_read_preferences() {
         preferences.getBytes("minima",servo_minimum,NUM_SERVOS);
     	preferences.getBytes("maxima",servo_maximum,NUM_SERVOS);
     }
-
-   if(preferences.getBytesLength("detach") != NUM_SERVOS) {
-    	Serial.println(F("Generating default detach adresses"));
-    	for (int i = 0; i < NUM_SERVOS; i++) {
-    		servo_detach_address[i] = i+7;
-    	}
-    	preferences.putBytes("detach",servo_detach_address,NUM_SERVOS);
-    } else {
-        preferences.getBytes("detach",servo_detach_address,NUM_SERVOS);
-    }
-
 
 	preferences.end();
 }
@@ -165,6 +142,35 @@ String processor(const String& var){
   if(var == "HOSTNAME"){
         return hostname;
     }
+    
+ if(var == "STATS"){
+ 		String table = "Shortest: "+String(((float)servo_packet_interval_min/1000.),2);
+		table += "ms | Mean: "+String((float)servo_packet_interval_avg/1000.,2);
+		table += "ms | Longest: "+String((float)servo_packet_interval_max/1000.,2) + "ms";
+		//reset stats
+		servo_packet_interval_min = 0;
+		servo_packet_interval_avg = 0;
+		servo_packet_interval_max = 0;
+        return table;
+ }
+
+
+ if(var == "LOG"){
+ 		String table = " ['Packet','Time'],";
+		// the following ensures using the right type for the index variable
+		using index_t = decltype(servo_packet_interval_log)::index_t;
+		float ms;
+		for (index_t i = 0; i < servo_packet_interval_log.size(); i++) {
+			ms = (float)servo_packet_interval_log[i]/1000.;
+			if (ms > 500.) ms = 500.;
+			table +="[" + String(i) + ",";
+			table += String(ms,2);
+			table += "]";
+			if ( i < servo_packet_interval_log.size()-1) 			table += ",";
+		}
+        return table;
+ }
+
  if(var == "SERVOS"){
  		String table = "";
  		
@@ -172,10 +178,10 @@ String processor(const String& var){
 
  			table += "<tr>";
  			table += "<th scope=\"row\">" + String(i + 1) + "</th>";
-      		table += "<td><input name =\"cc[" + String(i) + "]\" 		type=\"number\" 	maxlength=\"3\" size=\"3\" value=\""+  String(servo_address[i],DEC) +"\"></td>";
+      		table += "<td>"+ String(myservo[i].read()) + "</td>";
      		table += "<td><input  name =\"min[" + String(i) + "]\" 	type=\"number\" 	maxlength=\"3\" size=\"3\" value=\""+  String(servo_minimum[i],DEC) +"\"></td>";
 			table += "<td><input  name =\"max[" + String(i) + "]\" 	type=\"number\" 	maxlength=\"3\" size=\"3\" value=\""+  String(servo_maximum[i],DEC) +"\"></td>";
-			table += "<td><input  name =\"det[" + String(i) + "]\" 	type=\"number\" 	maxlength=\"3\" size=\"3\" value=\""+  String(servo_detach_address[i],DEC) +"\"></td>";
+			table += "<td>"+ String(servo_val_raw[i], DEC) +"</td>";
  			table += "<\tr>";
  		
  		}
@@ -206,6 +212,10 @@ void setup_web_server() {
         server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
             request->send(SPIFFS, "/index.html",  String(), false, processor);
         });
+
+        server.on("/log", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(SPIFFS, "/log.html",  String(), false, processor);
+        });
  
         server.on("/src/bootstrap.bundle.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
             request->send(SPIFFS, "/src/bootstrap.bundle.min.js", "text/javascript");
@@ -235,18 +245,14 @@ void setup_web_server() {
 					
 					if ((n >= 0) && (n < NUM_SERVOS)) {
 	
-						if (p->name().startsWith("cc")) { servo_address[n] = val; }
 						 if (p->name().startsWith("min")) { servo_minimum[n] = val; }
 						 if (p->name().startsWith("max")) { servo_maximum[n] = val; }
-						 if (p->name().startsWith("det")) { servo_detach_address[n] = val; }
 					}
             }
             
     		preferences.begin("changlier", false);
-    		preferences.putBytes("addresses",servo_address,NUM_SERVOS);
 			preferences.putBytes("minima",servo_minimum,NUM_SERVOS);
     		preferences.putBytes("maxima",servo_maximum,NUM_SERVOS);
-    		preferences.putBytes("detach",servo_detach_address,NUM_SERVOS);
   			preferences.end();
     
 			request->send(SPIFFS, "/index.html",  String(), false, processor);
@@ -279,18 +285,12 @@ void setup_web_server() {
 void service_servos(){
 	int	 val;
 	int i = 0;
-	if(!queue.isEmpty()) {
-		
-		val  = queue.pop();
-		val = map(val ,0,127,servo_minimum[i],servo_maximum[i]);
-		myservo[i].write(val);
-	}
-/*	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < NUM_SERVOS; i++) {
 		val  = servo_val_raw[i];
 		val = map(val ,0,127,servo_minimum[i],servo_maximum[i]);
 		myservo[i].write(val);
 	}
-	*/
+	last_servo_service = micros();
 }
 
 
@@ -311,18 +311,39 @@ void setup(){
     pinMode(LED_BUILTIN,OUTPUT);
     digitalWrite(LED_BUILTIN,HIGH);
 
- 	setup_read_preferences();
+	read_preferences();
  	setup_web_server();
 
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < NUM_SERVOS; i++) {
 		myservo[i].attach(servo_pins[i]);
 	}
 	
 	udp.begin(udpPort);
-
- 	t.every(2,service_servos);
 }
 
+void calc_stats() {
+	if (last_packet == 0) {
+			last_packet = micros();
+			return;
+	}
+	
+	servo_packet_interval = micros() - last_packet;
+	last_packet = micros();
+
+	servo_packet_interval_log.push(servo_packet_interval);
+
+	if (servo_packet_interval_avg == 0) {
+		servo_packet_interval_avg = servo_packet_interval;
+	} else {
+		servo_packet_interval_avg = (15 * servo_packet_interval_avg + servo_packet_interval) / 16;
+	}
+			
+	if (servo_packet_interval_min == 0) servo_packet_interval_min = servo_packet_interval;
+	if (servo_packet_interval_max == 0) servo_packet_interval_max = servo_packet_interval;
+
+	if (servo_packet_interval_min > servo_packet_interval) servo_packet_interval_min = servo_packet_interval;
+	if (servo_packet_interval_max < servo_packet_interval) servo_packet_interval_max = servo_packet_interval;
+}
 
 
 //========================================================================================
@@ -332,17 +353,25 @@ void setup(){
 void loop(){
 	uint8_t buffer[50];
 
-    t.update();
 
 	udp.parsePacket();
   	char len = udp.read(buffer, 50);
 	if( len > 0 ){
-		queue.push(buffer[0]);
-		last_packet = millis();
+		digitalWrite(LED_BUILTIN,HIGH);
+		
+		if (buffer[0] == 's') {					// servo message
+			calc_stats();
+			int numbytes = len - 1;
+			if (numbytes > NUM_SERVOS) numbytes = NUM_SERVOS;		// prevent reading garbage when receiving less bytes
+
+			for (int i = 0; i < numbytes; i++) {
+				servo_val_raw[i] = buffer[i + 1] - 1;
+			}
+		}
 	}
 	
-    if (millis() - last_packet > 100) {
-    	digitalWrite(LED_BUILTIN,LOW);
-    }
+    if ((micros() - last_packet)		 	> 200000) {					 	digitalWrite(LED_BUILTIN,LOW);}
+	if ((micros() - last_servo_service) 	> servo_service_interval) { 	service_servos(); }
+
 }
 
